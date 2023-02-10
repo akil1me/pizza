a0b6387860cff27d1f659ef3f08338a1e9c14272
akil1me <akipax123@gmail.com>
2023-02-10 23:55:09 +0500

    fix bug

--- src/components/pizza-list/pizza-list.jsx
+++ src/components/pizza-list/pizza-list.jsx
@@ -36,7 +36,7 @@ export const PizzaList = () => {
       </h2>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 md:gap-16 lg:grid-cols-3 xl:grid-cols-4 ">
-        {isLoading
+        {!isLoading
           ? pizzas?.map((pizza) => <PizzaItem key={pizza.id} {...pizza} />)
           : [...new Array(10)].map((_, index) => (
               <li className="" key={index} style={{ marginBottom: 50 }}>
